6b7fc208e997636b8143154553a6a05cc31687e0
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-04-06 14:46:27 -0300

    Testando delegate

---
+++ IntroductionDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using IntroductionDelegate.Services;
+
+namespace IntroductionDelegate;
+
+internal delegate double BinaryNumericOperation(double n1, double n2);
+
+public class Program
+{
+    static void Main(string[] agrs)
+    {
+        double a = 10;
+        double b = 12;
+
+
+        BinaryNumericOperation operation = CalculationService.Sum;
+
+        var result = operation(a, b);
+
+        Console.WriteLine(result);
+    }
+}
