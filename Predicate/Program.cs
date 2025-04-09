88a8ddd10efd80c90baaf40e326a35770ea6c64d
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-04-09 20:21:35 -0300

    Treinando Predicate

---
+++ Predicate/Program.cs
@@ -0,0 +1,28 @@
+﻿using Predicate.Entites;
+
+namespace Predicate;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new List<Product>();
+
+        list.Add(new Product("Tv", 900));
+        list.Add(new Product("Mouse", 50));
+        list.Add(new Product("Tablet", 350.50M));
+        list.Add(new Product("HD case", 80.90M));
+
+        list.RemoveAll(ProductTest);
+
+        foreach (Product p in list)
+        {
+            Console.WriteLine(p);
+        }
+    }
+
+    private static bool ProductTest(Product product)
+    {
+        return product.Price > 100;
+    }
+}
