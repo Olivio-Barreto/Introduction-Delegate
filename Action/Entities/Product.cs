0505a0d2f8f31da6506e4b5be7eed6079db74887
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-04-11 14:14:22 -0300

    Implementando Classe

---
+++ Action/Entities/Product.cs
@@ -0,0 +1,12 @@
+namespace Action.Entities;
+
+public class Product(string name, double price)
+{
+    public string Name { get; set; } = name;
+    public double Price { get; set; } = price;
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Price: {Price:F1}";
+    }
+}
