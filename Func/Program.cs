ba925273c9cf474696b2dff6bdd87cca206e240e
Olivio Barreto <Oliviobarreto49@gmail.com>
2025-04-27 12:31:11 -0300

    Projeto Finalizado

---
+++ Func/Program.cs
@@ -0,0 +1,26 @@
+﻿using Func.Entities;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new()
+        {
+            new Product("Tv", 3200.00),
+            new Product("Mouse", 50.00),
+            new Product("Macbook", 5600.00)
+        };
+
+        List<string> result = list.Select(NameUpper).ToList();
+
+        foreach (string item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string NameUpper(Product p)
+    {
+        return p.Name.ToUpper();
+    }
+}
